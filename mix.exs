af2b28676dbba4b270233134aee11f7a62987ee5
Pieter Schutte <schutte.pieter@gmail.com>
2019-10-09 21:22:32 +0200

    Most of chapter 6

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule ToyRobot.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: escript()
     ]
   end
 
@@ -16,6 +17,10 @@ defmodule ToyRobot.MixProject do
     [
       extra_applications: [:logger]
     ]
+  end
+
+  defp escript do
+    [main_module: ToyRobot.CLI]
   end
 
   # Run "mix help deps" to learn about dependencies.
---
+++ test/cli_test.exs
@@ -0,0 +1,47 @@
+defmodule ToyRobot.CLITest do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  alias ToyRobot.CLI
+
+  test "provides usage instructions if no arguments specified" do
+    output =
+      capture_io(fn ->
+        ToyRobot.CLI.main([])
+      end)
+
+    assert output |> String.trim() == "Usage: toy_robot commands.txt"
+  end
+
+  test "privdes usage instructions if too many arguments specified" do
+    output =
+      capture_io(fn ->
+        ToyRobot.CLI.main(["commands.txt", "commands2.txt"])
+      end)
+
+    assert output |> String.trim() == "Usage: toy_robot commands.txt"
+  end
+
+  test "shows an error mesage if the file does not exist" do
+    output =
+      capture_io(fn ->
+        ToyRobot.CLI.main(["i-don't-exist.txt"])
+      end)
+
+    assert output |> String.trim() == "The file i-don't-exist.txt does not exist"
+  end
+
+  test "handles commands and reports successfully" do
+    commands_path = Path.expand("test/fixtures/commands.txt", File.cwd!())
+
+    output = capture_io fn ->
+      ToyRobot.CLI.main([commands_path])
+    end
+
+    expected_output = """
+    The robot is at (0, 4) and is facing NORTH
+    """
+
+    assert output |> String.trim == expected_output |> String.trim
+  end
+end
